f653cec158640a9cbf5957d384959a1408e07e7e
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2023-04-17 16:22:50 +0100

    chore: add script to regenerate API on schema version update
    Fixes #12

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2023 NeuroML contributors
+# Author: NeuroML contributors
+# File : update.sh
+#
+# Script to regenerate and update the API for a new version of the NeuroML schema
+#
+NEUROML_VERSION="2.2"
+
+echo "Please ensure that you have ctest and xsd installed. See readme for details."
+
+rm -rf build && \
+    mkdir build && \
+    pushd build && \
+    cmake -DREGENERATE_BINDINGS=ON ../ && \
+    make && \
+    pushd src && sed -i -e 's/:param \(.*\):/@param \1/' -e '/:type/ d' -e '/\\n/ d' NeuroML_v${NEUROML_VERSION}.hxx && popd && \
+    make doc && make copy_wrappers &&
+    make test && popd && \
+    echo "All done"
